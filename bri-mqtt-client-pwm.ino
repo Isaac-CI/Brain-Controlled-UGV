cea83345192a72680c976ff6588c33d0b5b60de3
Isaac-CI <isaacmarinho@eng.ci.ufpb.br>
2024-05-06 18:36:22 -0300

    refactored and commented code for improved readability. Now PWM is not generated whenever the UGV is stopped

--- bri-mqtt-client-pwm.ino
+++ bri-mqtt-client-pwm.ino
@@ -13,114 +13,130 @@
 #define EN_A GPIO_NUM_22
 
 // pwm frequency
-#define LEDC_FREQ 2000
+#define PWM_FREQ 2000
 
 // WiFi parameters
-const char *ssid = "VIVOFIBRA-C9A8"; // Enter your WiFi name
+const char *ssid = "VIVOFIBRA-C9A8"; // Enter WiFi name
 const char *password = "83D24D124F";  // Enter WiFi password
 
 // MQTT Broker parameters
-const char *mqtt_broker = "192.168.15.120";
-const char *topic = "bri/command";
-const char *debug_topic = "bri/debug";
-const bool debug = false;
-const char *mqtt_username = "subscriber";
-const char *mqtt_password = "subscriber";
-const int mqtt_port = 1883;
+const char *mqtt_broker = "192.168.15.120"; // MQTT Broker is in localhost
+const char *topic = "bri/command";          // Sets MQTT topic to subscribe and receive mental commands
+const char *debug_topic = "bri/debug";      // Sets debug topic
+const bool debug = false;                   // Flag that controls debug logic
+const char *mqtt_username = "subscriber";   // Sets MQTT client username
+const char *mqtt_password = "subscriber";   // Sets MQTT client password
+const int mqtt_port = 1883;                 // Sets MQTT connection port
 
 // Setting PWM properties
-ledc_channel_config_t channel_EN_A = {
-  .gpio_num   =   EN_A,             // Seleciona o pino para atuar o PWM
-  .speed_mode =   LEDC_LOW_SPEED_MODE,   // Modo de Velocidade -> LOW
-  .channel    =   LEDC_CHANNEL_0,           
-  .timer_sel  =   LEDC_TIMER_0,
-  .duty       =   755,              // duty slightly bigger than EN_B in order to compesate uneven load
-  .hpoint     =   0
 
+// PWM duty cycle
+uint32_t dutyCycleL = 755;              // Duty Cycle for left motor. It is slightly bigger right motor duty cycle in order to compesate uneven load
+uint32_t dutyCycleR = 750;              // Duty Cycle for right motor
+
+ledc_channel_config_t channel_EN_A = {   // PWM channel config
+  .gpio_num   =   EN_A,                  // Selects PWM pin
+  .speed_mode =   LEDC_LOW_SPEED_MODE,   // Sets Speed Mode to LOW
+  .channel    =   LEDC_CHANNEL_0,        // Sets PWM channel
+  .timer_sel  =   LEDC_TIMER_0,          // Selects TIMER0 as the PWM timer
+  .duty       =   dutyCycleL,            // Sets the duty cylce
+  .hpoint     =   0                      // Sets the H point
 };
 
-ledc_channel_config_t channel_EN_B = {
-  .gpio_num   =   EN_B,             // Seleciona o pino para atuar o PWM
-  .speed_mode =   LEDC_LOW_SPEED_MODE,   // Modo de Velocidade -> LOW
-  .channel    =   LEDC_CHANNEL_1,           
-  .timer_sel  =   LEDC_TIMER_0,
-  .duty       =   750,
-  .hpoint     =   0
-
+ledc_channel_config_t channel_EN_B = {   // PWM channel config
+  .gpio_num   =   EN_B,                  // Selects PWM pin
+  .speed_mode =   LEDC_LOW_SPEED_MODE,   // Sets Speed Mode to LOW
+  .channel    =   LEDC_CHANNEL_1,        // Sets PWM channel
+  .timer_sel  =   LEDC_TIMER_0,          // Selects TIMER0 as the PWM timer
+  .duty       =   dutyCycleR,            // Sets the duty cylce
+  .hpoint     =   0                      // Sets the H point
 };
 
-//  Estrutura de dados para receber  as váriaveis  de configuração de frequência (1Hz, 100 Hz e outros), modo (HIGH ou LOW) e selecionar o TIMER (0,1,2)  
-
-ledc_timer_config_t timer = {                   // Configuração do timer 
-
-    .speed_mode      = LEDC_LOW_SPEED_MODE,     // Modo de Velocidade -> LOW
-    .duty_resolution = LEDC_TIMER_10_BIT,       // Resolução do do ciclo de trabalho (2^10 = 1024 valores)
-    .timer_num       = LEDC_TIMER_0,            // Utilizado o TIMER 0
-    .freq_hz         = LEDC_FREQ,               // Frequência de operação do sinal PWM
-    .clk_cfg         = LEDC_AUTO_CLK            // Seleção automatica da fonte geradora do clock (interna ou externa)
-  
+ledc_timer_config_t timer = {                   // Timer Config
+    .speed_mode      = LEDC_LOW_SPEED_MODE,     // Sets Speed Mode to LOW
+    .duty_resolution = LEDC_TIMER_10_BIT,       // Sets Duty Cycle resolution to 10 bits, i.e. ranging from 0 to 1023 
+    .timer_num       = LEDC_TIMER_0,            // Selects TIMER0 as the timer
+    .freq_hz         = PWM_FREQ,                // Sets PWM frequency
+    .clk_cfg         = LEDC_AUTO_CLK            // Automatic selection of clock generation source
 };
 
-uint32_t dutyCycleL = 755;
-uint32_t dutyCycleR = 750;
+WiFiClient espClient;           // WiFI client helper
+PubSubClient client(espClient); // MQTT client helper
 
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-typedef enum {
+typedef enum { // Enum type for improved readability
   foward,
   rotate_clockwise,
-  rotate_anticlockwise,
+  rotate_counterclockwise,
   stop,
   continue_movement
-} movement_t;
+} command_t;
 
-void switch_movement(movement_t mode) {
-  switch(mode) {
+// Enum type variable to track commands
+command_t command;
+
+/*
+ * Parameters: command_t command: Command indicating the UGV how to move in the next cycle
+ * This function switchs the received command and sets pin outputs accordingly
+ * Returns: None
+*/
+void switch_movement(command_t command) {
+  switch(command) {
+    // Command = foward
     case 0:
-      // ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, dutyCycleL, 0);
-      // ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, dutyCycleR, 0);
+      ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, dutyCycleL, 0);
+      ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, dutyCycleR, 0);
       gpio_set_level(IN_1, 1);
       gpio_set_level(IN_2, 0);
       gpio_set_level(IN_3, 1);
       gpio_set_level(IN_4, 0);
       break;
+    // Command = rotate_clockwise
     case 1:
-      // ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, dutyCycleL, 0);
-      // ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, 0, 0);
+      ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, dutyCycleL, 0);
+      ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, 0, 0);
       gpio_set_level(IN_1, 1);
       gpio_set_level(IN_2, 0);
       gpio_set_level(IN_3, 0); 
       gpio_set_level(IN_4, 0);
       break;
-    case 2:        
-      // ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 0, 0);
-      // ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, dutyCycleR, 0);  
+    // Command = rotate_counterclockwise
+    case 2:
+      ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 0, 0);
+      ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, dutyCycleR, 0);  
       gpio_set_level(IN_1, 0);
       gpio_set_level(IN_2, 0);
       gpio_set_level(IN_3, 1);
       gpio_set_level(IN_4, 0);
       break;
-    case 3:        
-      // ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 0, 0);
-      // ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, 0, 0);  
+    // Command = stop
+    case 3:
+      ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 0, 0);
+      ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, 0, 0);  
       gpio_set_level(IN_1, 0);
       gpio_set_level(IN_2, 0);
       gpio_set_level(IN_3, 0);
       gpio_set_level(IN_4, 0);
       break;
+    // Command is continue movement or a value that is not supported
     default:
       // do nothing
       break;
   }
 }
 
+/*
+ * Parameters: None
+ * This function configures output pins and PWM channels 
+ * Returns: None
+*/
 void set_pins() {
-  ledc_timer_config(&timer); // Envia o endereço  da estrutura timer para a função de configuração do canal PWM 
+  ledc_timer_config(&timer); // Sends timer to PWM channel configuration
 
+  // Configure PWM channels
   ledc_channel_config(&channel_EN_A);
   ledc_channel_config(&channel_EN_B);
 
+  // Starts PWM generation
   ledc_fade_func_install(0);
 
   // Configure the GPIO ports as outputs  
@@ -134,6 +150,11 @@ void set_pins() {
   gpio_set_direction(IN_4, GPIO_MODE_OUTPUT);
 }
 
+/*
+ * Parameters: None
+ * This function handles MQTT connection, connecting the UGV client to the designated broker and ~]]~; 
+ * Returns: None
+*/
 void connect_mqtt() {
   // set mqtt broker parameters
   client.setServer(mqtt_broker, mqtt_port);
@@ -218,27 +239,30 @@ void callback(char *topic, byte *payload, unsigned int length) {
   char message[length + 1];
   read_mqtt_message(topic, payload, length, message);
   
-  movement_t move = stop;
+  // Default movement is to stop, in order to stop the UGV in case of receiving an usuppoted command 
+  command = stop;
 
   // Verifiy which command was received
-  if(!strcmp(message, "pull")) { move = stop; }
-  else if(!strcmp(message, "push")) { move = foward; }
-  else if(!strcmp(message, "lift")) { move = rotate_clockwise; }
-  else if(!strcmp(message, "drop")) { move = rotate_anticlockwise; }
-  else { move = stop; }
+  if(!strcmp(message, "pull")) { command = stop; }
+  else if(!strcmp(message, "push")) { command = foward; }
+  else if(!strcmp(message, "lift")) { command = rotate_clockwise; }
+  else if(!strcmp(message, "drop")) { command = rotate_counterclockwise; }
+  else if(!strcmp(message, "neutral")) { command = continue_movement; }
+  // else { command = stop; }
   
   // Update pin output accordingly to the received command
-  switch_movement(move);
+  switch_movement(command);
   if(debug) { client.publish(debug_topic, "-----------------------"); }
 }
 
 void loop() {
   if (!client.connected()) { // if mqtt client was disconnected
+    command = stop;
+    switch_movement(command);
     if (WiFi.status() == WL_CONNECTED) { // connects to mqtt client if WiFi is connected
       connect_mqtt();
-    } else { // or waits until WiFi reconnection
-      // TO DO: parar o robô em caso de desconexão
-      delay(2000);
+    } else { // waits for WiFi reconnection
+      delay(200);
     }
   } else {
     // Client connected
